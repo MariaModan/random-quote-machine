186cec4c0d621432fc4edf637712231aa99af0b6
Maria <maria.modan95@gmail.com>
2019-08-11 14:45:40 +0300

    cerate react app and write page structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Quote from './Quote';
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>Random Quote Generator</h1>
+          <p>Inspiration at the click of a button</p>
+          </header>
+          <Quote />
+      </div>
+    )
+  }
+  
+}
+
+export default App;
---
+++ src/Quote.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Quote extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return(
+            <div>
+                <h3>inspirtaional quote</h3>
+                <button>Next Quote</button>
+            </div>
+        )
+    }
+}
+
+export default Quote;
